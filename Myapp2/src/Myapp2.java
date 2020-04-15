2d682ce4e667d8a16dd8b25aecad0978a61a55d1
Norhan147 <nmohamed1472000@yahoo.com>
2020-04-15 22:07:00 +0200

    initial commit

---
+++ Myapp2/src/Myapp2.java
@@ -0,0 +1,14 @@
+
+public class Myapp2 {
+
+	public Myapp2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO EVERYONE");
+
+	}
+
+}
---
+++ Myapp3/src/Myapp3.java
@@ -0,0 +1,14 @@
+
+public class Myapp3 {
+
+	public Myapp3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
